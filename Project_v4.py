90c00a6654fd51aa5423cbbac4a4585463ff8680
JHVH <chenjinhua00@gmail.com>
2023-11-06 01:20:45 +0000

    Falta concluir o ramo do decimal para binário

---
+++ Project_v4.py
@@ -0,0 +1,162 @@
+#################################### FUNÇÕES###########################################
+def menu_principal():
+    print("*****************************************************************************************")
+    print("*   Benvido ao nosso programa de cálculo!                                               *")
+    print("*                                                                                       *")
+    print("*   A nossa calculadora é capaz de converter números binários em decimais e vice-versa! *")
+    print("*   Esperamos que goste!                                                                *")
+    print("*   Se precisar de ajuda, escreva 'help'                                                *")
+    print("*                                                                                       *")
+    print("*****************************************************************************************")
+    print("*                                                                                       *")
+    print("*   Realizado por: Artur Alves e Jinhua Chen                                            *")
+    print("*   Turma: 12ºCE                                                                        *")
+    print("*   Escola: Colégio Vasco da Gama                                                       *")
+    print("*   Disciplina: AIB                                                                     *")
+    print("*   Professor: Pedro Silva                                                              *")
+    print("*                                                                         Versão: 1.0   *")
+    print("*                                                                          08/11/2023   *")
+    print("*****************************************************************************************")
+
+
+def opcao_a():
+    binario = input("Escreve um número binário:\n")
+    tamanho = len(binario)
+    while not ((all(c in '01' for c in binario)) or (binario[0] == "-" and all(c in '01' for c in binario[1:]))):
+        print("Por favor, digite um número válido!")
+        binario = input("Escreve um número binário:\n")
+
+    if binario[0] == "-":
+        binario = binario[1:]
+        decimal = "-"+binario_to_decimal(binario, tamanho-1)
+        print("O número decimal correspondente é", decimal, "\n")
+    else:
+        tamanho = len(binario)  # atualizar o tamanho
+        print("O número decimal correspondente é",
+              binario_to_decimal(binario, tamanho), "\n")
+
+
+def opcao_b():
+    decimal = input("Escreve um número decimal:\n")
+    if not (ft_isdigit(decimal) or (decimal[0] == "-" and ft_isdigit(decimal[1:]))):
+        print("Por favor, digite um número válido!")
+        # continue
+    elif int(decimal) > 0:
+        decimal = int(decimal)
+        print("O número binário correspondente é",
+              decimal_to_binario(decimal), "\n")
+    else:
+        decimal = int(decimal)
+        binario = "-"+decimal_to_binario(abs(decimal))
+        print("O número binário correspondente é", binario, "\n")
+
+
+def binario_to_decimal(binario, tamanho):
+    result = 0
+    for c in binario:
+        c = int(c)
+        result += c * 2 ** (tamanho - 1)
+        tamanho -= 1
+    return str(result).strip()
+
+
+def decimal_to_binario(decimal):
+    result = list()
+    while decimal > 0:
+        result.append(decimal % 2)
+        decimal = decimal // 2
+    result.reverse()
+    n = 0
+    # passar de list para int
+    for numero in result:
+        n = n * 10 + numero
+    return str(n).strip()
+
+# Passa o str para int. Se conseguir, retorna True, se não, retorna False
+
+
+def ft_isdigit(str):
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
+
+
+def ft_escolha_uma_opção():
+    print("Escolha uma opção:")
+    print("(A)Continuar o cálculo")
+    print("(B)Sair")
+    opção = input()
+
+    while opção.lower() != "a" and opção.lower() != "b":
+        print("Por favor, escolha uma opção válida!")
+
+    if opção.lower() == "a":
+        return True
+    elif opção.lower() == "b":
+        return False
+
+
+def check_option(opção):
+    while opção.lower() != "a" and opção.lower() != "b" and opção.lower() != "c":
+        print("Por favor, escolha uma opção válida!")
+        print("Escolha uma opção:")
+        print("(A)Continuar o cálculo")
+        print("(B)Voltar ao menu principal")
+        print("(C)Sair")
+        opção = input()
+    if opção.lower() == "a":
+        return "a"
+    elif opção.lower() == "b":
+        return "b"
+    elif opção.lower() == "c":
+        return "c"
+
+
+########################################### MAIN FUNCTION #########################################
+while True:
+    menu_principal()
+    option = input(
+        "Escolha uma opção:\n(A)Binário para decimal\n(B)Decimal para binário\n(C)Sair\n")
+    while option.lower() != "a" and option.lower() != "b" and option.lower() != "c":
+        print("Por favor, escolha uma opção válida!")
+        option = input(
+            "Escolha uma opção:\n(A)Binário para decimal\n(B)Decimal para binário\n(C)Sair\n)")
+
+    if option.lower() == "a":
+        opcao_a()
+    elif option.lower() == "b":
+        opcao_b()
+    elif option.lower() == "c":
+        break
+
+    print("Escolha uma opção:")
+    print("(A)Continuar o cálculo")
+    print("(B)Voltar ao menu principal")
+    print("(C)Sair")
+    opção = input()
+
+    while opção.lower() != "a" and opção.lower() != "b" and opção.lower() != "c":
+        print("Por favor, escolha uma opção válida!")
+        print("Escolha uma opção:")
+        print("(A)Continuar o cálculo")
+        print("(B)Voltar ao menu principal")
+        print("(C)Sair")
+        opção = input()
+
+    while opção.lower() == "a":
+        opcao_a()
+        print("Escolha uma opção:")
+        print("(A)Continuar o cálculo")
+        print("(B)Voltar ao menu principal")
+        print("(C)Sair")
+        opção = input()
+        opção = check_option(opção)
+
+    if opção.lower() == "b":
+        continue
+    elif opção.lower() == "c":
+        break
+
+print("Obrigado por calcular connosco!")
